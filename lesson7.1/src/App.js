59fdfa35c1bc98b00335ca19ff75a81933d40f50
Jacob DiTrolio <jad7136@psu.edu>
2024-04-10 20:51:22 -0400

    2nd commit

--- lesson7.1/src/App.js
+++ lesson7.1/src/App.js
@@ -3,23 +3,23 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Movie from "./movie.js"
 import React from 'react';
 import './movie.css';
+//This uses 3 movies components in App
 function App() {
   return (
-    <div className="App">
-      <h1>List of movies</h1>
-      <div className= "movies">
-      <div>
+    <div className="App"> 
+      <h1>List of Movies</h1>
+      <div className= "movies row">
+        <div className='col-lg-4  col-md-6 col-sm-12'>
+        <Movie/>
+        </div>
+        </div>
+        <div className='col-lg-4  col-md-6 col-sm-12'>
         <Movie/>
       </div>
-        <div>
-          <Movie/>
-        </div>
-        <div>
+      <div className='col-lg-4  col-md-6 col-sm-12'>
           <Movie/>
         </div>
       </div>
-    </div>
   );
 }
-
 export default App;
--- lesson7.1/src/movie.js
+++ lesson7.1/src/movie.js
@@ -1,9 +1,9 @@
 function Movie(){
     return(
     <div className="Movie">
-        <h2>movie</h2>
-        <h4>hi</h4>
-        <p>s</p>
+        <h2>Movie Title: The Batman</h2>
+        <h4>Director: Matt Reeves</h4>
+        <p>Stars: Robert pattinson, Zoe Kravitz</p>
     </div>
     );
 }
